7a8e3e9366290661dce2acda56eb596c87fdabe0
ariel klevecz <ariel@yaytso.art>
2025-04-02 00:48:19 -0700

    more img

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -47,7 +47,6 @@
 	<meta name="twitter:card" content="summary_large_image" />
 	<meta name="twitter:title" content={seo.title} />
 	<meta name="twitter:description" content={seo.description} />
-	<meta name="twitter:image" content={seo.image} />
 	<meta property="og:title" content={seo.title} />
 	<meta property="og:description" content={seo.description} />
 	<!-- <meta property="og:image" content={seo.image} /> -->
