0269dbab51657f18c4c3df6d048b322a54e4c35b
ariel klevecz <ariel@yaytso.art>
2025-04-06 14:48:27 -0700

    better event param checking

--- src/routes/+layout.server.js
+++ src/routes/+layout.server.js
@@ -1,24 +1,35 @@
 import { EVENT_ID, getOrCreateCart, mockProducts, validEventNames } from '$lib';
 import { defaultCart } from '$lib/stores/cart.svelte';
 
+/** @param {string} eventName */
+function isValidEventName(eventName) {
+	return validEventNames.includes(eventName);
+}
+
 /** @type {import('./$types').LayoutServerLoad} */
 export async function load({ cookies, url, locals, params }) {
 	const cart = getOrCreateCart({ cookies, defaultCart });
 	const userSession = JSON.parse(cookies.get('user') || '{}');
 	const token = cookies.get('token') || '';
 
-	let currentEvent = cookies.get('event') || EVENT_ID;
-	if (validEventNames.includes(currentEvent) === false) {
-		currentEvent = EVENT_ID
-	}
+	let currentEvent = EVENT_ID;
 	if (params.eventName) {
-		currentEvent = params.eventName;
-		cookies.set('event', currentEvent, {
-			path: '/',
-			httpOnly: false,
-			maxAge: 60 * 60 * 24 * 30
-		});
+		if (isValidEventName(params.eventName)) {
+			currentEvent = params.eventName;
+
+			cookies.set('event', currentEvent, {
+				path: '/',
+				httpOnly: false,
+				maxAge: 60 * 60 * 24 * 30
+			});
+		}
+	} else {
+		const eventCookie = cookies.get('event');
+		if (eventCookie && isValidEventName(eventCookie)) {
+			currentEvent = eventCookie;
+		}
 	}
+
 	const featured = mockProducts.find((p) => p.id === currentEvent);
 	return { cart, user: userSession, products: mockProducts, token, featured: featured };
 }
