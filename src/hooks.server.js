3a9646ed8791c5f9a2e44f3058f408b6c075f488
ariel klevecz <ariel@yaytso.art>
2025-04-19 13:37:10 -0700

    fix sentry bs

--- src/hooks.server.js
+++ src/hooks.server.js
@@ -1,17 +1,26 @@
 import { sequence } from '@sveltejs/kit/hooks';
-import * as Sentry from '@sentry/sveltekit';
+import { initCloudflareSentryHandle, sentryHandle } from '@sentry/sveltekit';
 import { PUBLIC_SENTRY_ENDPOINT } from '$env/static/public';
 import { dev } from '$app/environment';
 
-Sentry.init({
-	dsn: PUBLIC_SENTRY_ENDPOINT,
-	environment: dev ? 'development' : 'production',
-	tracesSampleRate: 1
-});
+// Sentry.init({
+// 	dsn: PUBLIC_SENTRY_ENDPOINT,
+// 	environment: dev ? 'development' : 'production',
+// 	tracesSampleRate: 1
+// });
 
-/** @type {import('@sveltejs/kit').Handle} */
-export const handle = sequence(Sentry.sentryHandle(), async ({ event, resolve }) => {
-	const response = await resolve(event);
-	return response;
-});
-export const handleError = Sentry.handleErrorWithSentry();
+// /** @type {import('@sveltejs/kit').Handle} */
+// export const handle = sequence(Sentry.sentryHandle(), async ({ event, resolve }) => {
+// 	const response = await resolve(event);
+// 	return response;
+// });
+// export const handleError = Sentry.handleErrorWithSentry();
+
+export const handle = sequence(
+	initCloudflareSentryHandle({
+		dsn: PUBLIC_SENTRY_ENDPOINT,
+		environment: dev ? 'development' : 'production',
+		tracesSampleRate: 1.0
+	}),
+	sentryHandle()
+);
