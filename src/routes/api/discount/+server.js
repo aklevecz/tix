e82e7d97bd5d5864b6a7eed9002e294726b815e0
ariel klevecz <ariel@yaytso.art>
2025-06-30 12:31:36 -0700

    up

--- src/routes/api/discount/+server.js
+++ src/routes/api/discount/+server.js
@@ -2,14 +2,14 @@ import { json } from '@sveltejs/kit';
 
 /** @type {{ [key: string]: number }} */
 const discounts = {
-	'livelaughraptor': 25,
-	'idunnohowtariffswork': 50
-}
+	livelaughraptor: 25,
+	idunnohowtariffswork: 50
+};
 /** @type {import('./$types').RequestHandler} */
 export async function POST({ request }) {
 	const { discountCode } = await request.json();
 
-	const discount = discounts[discountCode]
+	const discount = discounts[discountCode];
 	if (discount) {
 		return json({ discountCode, discount });
 	}
